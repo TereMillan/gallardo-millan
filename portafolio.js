3fe103d8e0a9cded2227cb9b005e43eb298f171a
TereMillan <42349035+TereMillan@users.noreply.github.com>
2019-04-22 13:39:22 -0400

    Add files via upload

---
+++ portafolio.js
@@ -0,0 +1,43 @@
+// PRIMERO CREAMOS UNA VARIABLE QUE CONTIENE TODOS LOS DATOS DE TUS TRABAJOS
+var trabajos = [
+{
+    figure: "media/foto-01.jpg",
+    title: "Montípora",
+    about: "Luminaria de cera de vela, que a través de su textura y luz provoca un ambiente de relajo.",
+}, {
+    figure: "media/foto-02.jpeg",
+    title: "Nekochi",
+    about: "Marca de producto para evacuar gatos con urgencia.",
+}, {
+    figure: "media/foto-03.jpg",
+    title: "Bisutería",
+    about: " Porcelana en frio y marmolado, trabajo en conjunto con micro emprendedora.",
+}, {
+    figure: "media/foto-04.png",
+    title: "Ilustraciones",
+      about: "",
+
+}, {
+    figure: "media/foto-05.jpg",
+    title: "Apa",
+    about: "Dispositivo para cargar fácilmente a personas con movilidad reducida.",
+}, {
+    figure: "media/foto-06.png",
+    title: "Contacto Espacial",
+    about: "Juego de mesa no dependiente del sentido de la vista.",
+},
+
+{
+    figure: "media/foto-07.jpeg",
+    title: "Domésticxs",
+    about: "Delantales de jeans reciclados, trabajo en conjunto con micro emprendedora.",
+},
+
+]
+// LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
+$.each(trabajos, function(i, val) {
+    var uno = '<figure><img src="' + trabajos[i].figure + '"/></figure>';
+    var dos = '<h4>' + trabajos[i].title + '</h4>';
+    var tres = '<p>' + trabajos[i].about + '</p>';
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+});
